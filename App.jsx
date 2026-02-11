fd38cbe3827915d2793c64b1d360c34e061b87b6
zehra nisar <zehranisar75@gmail.com>
2026-02-11 19:29:45 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+import Trusted from "./components/Trusted";
+import CaseStudies from "./components/CaseStudies";
+import Blog from "./components/Blog";
+import Footer from "./components/Footer";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Services />
+      <Trusted />
+      <CaseStudies />
+      <Blog/>
+      <Footer/>S
+    </>
+  );
+}
+
+export default App;
